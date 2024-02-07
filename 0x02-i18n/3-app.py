2521985ce3f82f04fa7a12c9b70a62ec9d5a110e
Jealous Matsikachando <pasej5@gmail.com>
2024-02-07 03:56:39 +0200

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,40 +1,46 @@
 #!/usr/bin/env python3
+"""A simple flask app
 """
-Parametrize templates
-"""
+
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config(object):
-    """
-    setting babel's default locale
+    """_summary_
+
+    Returns:
+            _type_: _description_
     """
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
+    """_summary_
+
+    Returns:
+            _type_: _description_
     """
-    Get locale from request
-    """
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
-def index_html() -> str:
-    """
-    Basic Flask app and handles the / route
+@app.route('/')
+def index():
+    """_summary_
     """
     return render_template('3-index.html')
 
 
-if __name__ == "__main__":
-    app.run(port='5000', host='0.0.0.0', debug=True)
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
