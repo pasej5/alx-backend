fe898f27bb7a6706517388ab2b25798ea772be0d
Jealous Matsikachando <pasej5@gmail.com>
2024-02-07 05:06:30 +0200

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -8,10 +8,10 @@ from flask_babel import Babel
 
 
 class Config(object):
-    """_summary_ configuration
+    """_summary_
 
     Returns:
-            _type_: _description_
+                    _type_: _description_
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -24,6 +24,7 @@ app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -31,30 +32,30 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+
 def get_user():
+    """returns a user dictionary or None if the ID cannot be found
     """
-    Function That returns a dictionary or none
-    if the ID cannot be found 
-    """
-    login_id = request.args.get("login_as")
+    login_id = request.args.get('login_as')
     if login_id:
-        return users.args(int(login_id))
+        return users.get(int(login_id))
     return None
+
 
 @app.before_request
 def before_request() -> None:
-    """
-    function executed before all other functions
+    """_summary_
     """
     user = get_user()
     g.user = user
+
 
 @babel.localeselector
 def get_locale():
     """_summary_
 
     Returns:
-            _type_: _description_
+                    _type_: _description_
     """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
@@ -62,6 +63,8 @@ def get_locale():
         return locale
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+# babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
