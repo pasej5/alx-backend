de093b82bb5f9b1fcd09d156cbca1306f968d4a6
Jealous Matsikachando <pasej5@gmail.com>
2024-03-04 23:03:21 +0200

    Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,31 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+function redisConnect() {
+  client
+    .on('connect', function () {
+      console.log('Redis client connected to the server');
+    })
+    .on('error', (err) => {
+      console.log(`Redis client not connected to server: ${err}`);
+    });
+}
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, function (error, result) {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+    console.log(result);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
