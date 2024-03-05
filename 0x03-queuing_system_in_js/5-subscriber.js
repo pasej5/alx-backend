393e42feddb605017c844db16e118866a9d6771e
Jealous Matsikachando <pasej5@gmail.com>
2024-03-05 11:29:08 +0200

    publisher

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,24 +1,21 @@
 import { createClient } from 'redis';
 
-const publisher = createClient();
+const subscriber = createClient();
 
-//connect to the server
-publisher.on('connect', function () {
+subscriber.on('connect', function () {
   console.log('Redis client connected to the server');
 });
-publisher.on('error', function (error) {
+
+subscriber.on('error', function (error) {
   console.log(`Redis client not connected to the server: ${error.message}`);
 });
 
-//function to subscribe to the holberton school channel
-function publishMessage(message, time) {
-  setTimeout(function () {
-    console.log(`About to send ${message}`);
-    publisher.publish('holberton school channel', message);
-  });
-}
+subscriber.subscribe('holberton school channel');
 
-publishMessage('Holberton Student #1 starts course', 100);
-publishMessage('Holberton Student #2 starts course', 200);
-publishMessage('KILL_SERVER', 300);
-publishMessage('Holberton Student #3 starts course', 400);
+subscriber.on('message', function (channel, message) {
+  console.log(`Received message '${message}' on channel '${channel}'`);
+  if (message === 'KILL_SERVER') {
+    subscriber.unsubscribe();
+    subscriber.quit();
+  }
+});
